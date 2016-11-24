51a84bd0edac3cd24c4f7d57f375281e552459ec
Lokki <df1985@mail.ru>
2016-11-24 22:51:35 +0300

    work

--- src/main/java/ru/restaurant/web/user/UserRestController.java
+++ src/main/java/ru/restaurant/web/user/UserRestController.java
@@ -3,8 +3,7 @@ package ru.restaurant.web.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.restaurant.model.User;
 import ru.restaurant.service.UserService;
 import ru.restaurant.web.AuthorizedUser;
@@ -26,16 +25,19 @@ public class UserRestController {
         return service.get(id, AuthorizedUser.getId());
     }
 
-    public User create(User user) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public User create(@RequestBody User user) {
         return service.save(user, AuthorizedUser.getId());
     }
 
-    public void delete(int id) {
+    @DeleteMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") int id) {
         service.delete(id, AuthorizedUser.getId());
     }
 
-    public void update(User user, int id) {
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public User update(@RequestBody User user, @PathVariable int id) {
         user.setId(id);
-        service.save(user, AuthorizedUser.getId());
+        return service.save(user, AuthorizedUser.getId());
     }
 }
