06e19513340d6dfe31137c8e045ee75a560adb4e
lokki17 <df1985@mail.ru>
2016-11-27 09:41:47 +0300

    init

--- src/main/java/ru/restaurant/util/TimeUtil.java
+++ src/main/java/ru/restaurant/util/TimeUtil.java
@@ -5,13 +5,13 @@ import java.time.LocalTime;
 public class TimeUtil {
     public static final LocalTime STOP_TIME = LocalTime.of(11, 0);
     public static final LocalTime LANCH_TIME = LocalTime.of(15, 0);
-    public static final LocalTime FAKE_LANCH_TIME = LocalTime.of(23, 0);
+//    public static final LocalTime LANCH_TIME = LocalTime.of(23, 0);
 
     public static boolean checkTime(LocalTime localTime){
         return localTime.isAfter(STOP_TIME);
     }
 
     public static boolean checkLaunchTime(LocalTime localTime){
-        return localTime.isAfter(FAKE_LANCH_TIME);
+        return localTime.isAfter(LANCH_TIME);
     }
 }
