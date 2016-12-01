2da837705069f179e737f8ba82c52590e71d7571
Lokki <df1985@mail.ru>
2016-12-01 13:54:06 +0300

    work

---
+++ src/main/java/ru/restaurant/util/exception/AccessDeniedHandlerImpl.java
@@ -0,0 +1,27 @@
+package ru.restaurant.util.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import ru.restaurant.web.ExceptionInfoHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AccessDeniedHandlerImpl extends ExceptionInfoHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(new ErrorInfo(request.getRequestURL(), accessDeniedException));
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+        response.getWriter().write(s);
+        response.getWriter().flush();
+        response.getWriter().close();
+
+    }
+
+}
