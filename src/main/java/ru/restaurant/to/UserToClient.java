fa9cffac3a319611c4ee263bc10378bda77a2195
Lokki <df1985@mail.ru>
2016-12-01 16:30:53 +0300

    work

--- src/main/java/ru/restaurant/to/UserToClient.java
+++ src/main/java/ru/restaurant/to/UserToClient.java
@@ -1,15 +1,20 @@
 package ru.restaurant.to;
 
+import ru.restaurant.model.Role;
 import ru.restaurant.model.User;
+
+import java.util.Set;
 
 public class UserToClient {
 
     private final Integer id;
     private final String name;
+    private final Set<Role> roles;
 
     public UserToClient(User user) {
         this.id = user.getId();
         this.name = user.getName();
+        this.roles = user.getRoles();
     }
 
     public Integer getId() {
@@ -18,5 +23,9 @@ public class UserToClient {
 
     public String getName() {
         return name;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
