8c8e0d109f5da28871b3a19a56ea39540216b908
lokki17 <df1985@mail.ru>
2016-12-04 10:27:44 +0300

    init

---
+++ src/test/ru/restaurant/service/DishTestData.java
@@ -0,0 +1,4 @@
+package ru.restaurant.service;
+
+public class DishTestData {
+}
---
+++ src/test/ru/restaurant/service/jpa/DishServiceImplTest.java
@@ -0,0 +1,34 @@
+package ru.restaurant.service.jpa;
+
+import org.junit.Test;
+import ru.restaurant.service.AbstractServiceTest;
+
+import static org.junit.Assert.*;
+
+public class DishServiceImplTest extends AbstractServiceTest{
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllOnDate() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+}
