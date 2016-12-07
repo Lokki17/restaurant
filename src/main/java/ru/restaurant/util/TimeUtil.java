7b8b9fe189bcac18009dcbd51fe98fbfe23fc3a3
Lokki <df1985@mail.ru>
2016-12-07 11:47:46 +0300

    work

--- src/main/java/ru/restaurant/util/TimeUtil.java
+++ src/main/java/ru/restaurant/util/TimeUtil.java
@@ -13,8 +13,6 @@ public class TimeUtil {
     public static final LocalTime STOP_TIME = LocalTime.of(21, 0);
     public static final LocalTime LUNCH_TIME = LocalTime.of(22, 0);
 
-
-
     public static void checkTime(LocalTime localTime) {
         if (localTime.isAfter(STOP_TIME)) {
             throw new WrongTimeException("You have made your choice today");
