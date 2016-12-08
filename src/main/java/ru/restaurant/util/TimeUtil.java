0d8ceaefda2496077c7c06ffc497bdcd6c2509d7
Lokki <df1985@mail.ru>
2016-12-08 11:16:35 +0300

    work

--- src/main/java/ru/restaurant/util/TimeUtil.java
+++ src/main/java/ru/restaurant/util/TimeUtil.java
@@ -10,7 +10,7 @@ public class TimeUtil {
     private TimeUtil() {
     }
 
-    public static final LocalTime STOP_TIME = LocalTime.of(21, 0);
+    public static final LocalTime STOP_TIME = LocalTime.of(11, 0);
     public static final LocalTime LUNCH_TIME = LocalTime.of(22, 0);
 
     public static void checkTime(LocalTime localTime) {
